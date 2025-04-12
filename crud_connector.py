69d231b17b716f500699bdbd8a42a7ca392353f8
jonathanM-37 <jonathanmendezmartinez37@gmail.com>
2025-04-12 17:06:42 -0600

    segundo commit..

--- crud_connector.py
+++ crud_connector.py
@@ -4,7 +4,7 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="papu19",
+        password="contraseña",
         database="tiendadelsol"
     )
 # CRUD articulos
